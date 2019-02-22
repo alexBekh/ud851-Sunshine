efa2d64a69fb3d5c4ccb494fc6478a5653f5f9a9
iBjors <alex.bekhelev@gmail.com>
2019-02-23 00:01:02 +0300

    S10.01 Move fetching data from network to IntentService.

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,4 +1,45 @@
+package com.example.android.sunshine.sync;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+
+import com.example.android.sunshine.data.WeatherContract;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+
+import java.net.URL;
+
 //  TODO (1) Create a class called SunshineSyncTask
+class SunshineSyncTask
+{
+    public static synchronized void syncWeather(Context context)
+    {
+        ContentValues[] valuesFromJson;
+        try
+        {
+            final URL url = NetworkUtils.getUrl(context);
+            final String responseFromServer = NetworkUtils.getResponseFromHttpUrl(url);
+            valuesFromJson = OpenWeatherJsonUtils.getWeatherContentValuesFromJson(context, responseFromServer);
+    
+            if (valuesFromJson != null && valuesFromJson.length > 0)
+            {
+                final ContentResolver contentResolver = context.getContentResolver();
+                int itemsDeleted = contentResolver.delete(WeatherContract.WeatherEntry.CONTENT_URI, null, null);
+                if (itemsDeleted == -1)
+                    throw new Exception("Can't delete old data from content provider");
+        
+                int itemsInserted = contentResolver.bulkInsert(WeatherContract.WeatherEntry.CONTENT_URI, valuesFromJson);
+                if (itemsInserted != valuesFromJson.length)
+                    throw new Exception("Can't insert new data to content provider");
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
 //  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
 //      TODO (3) Within syncWeather, fetch new weather data
 //      TODO (4) If we have valid results, delete the old data and insert the new
